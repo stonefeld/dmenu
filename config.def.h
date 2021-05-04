f93db7cb37bc0b11c801565a512847d8b0eb4cd4
stonefeld - Theo Stanfield <theo.stanfield@gmail.com>
2021-05-04 11:35:07 -0300

    added colored emojis support

--- config.def.h
+++ config.def.h
@@ -7,13 +7,15 @@ static int min_width = 500;                    /* minimum width when centered */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"SauceCodePro Nerd Font:size=11"
+	"Source Code Pro:size=11",
+    "JoyPixels:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#d8dee9", "#2e3440" },
-	[SchemeSel] = { "#3b4252", "#81a1c1" },
+	/* [SchemeSel] = { "#3b4252", "#81a1c1" }, */
+	[SchemeSel] = { "#3b4252", "#88c0d0" },
 	/* [SchemeSel] = { "#3b4252", "#ebcb8b" }, */
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
